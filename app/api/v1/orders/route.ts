bd42cff5ddfd63ff68c6593ba32c5b0c97b8beac
Abdus samad <abdussamadkotha@gmail.com>
2026-07-29 15:04:28 +0500

    deployment issue fixed

--- app/api/v1/orders/route.ts
+++ app/api/v1/orders/route.ts
@@ -66,7 +66,8 @@ export async function POST(request: Request) {
             quantity: item.quantity,
             unitPrice: item.unitPrice,
             totalPrice: item.totalPrice,
-            specialInstructions: item.specialInstructions,
+            // specialInstructions: item.specialInstructions,
+            
           }
         })
 

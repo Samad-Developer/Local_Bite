4d53c6ab52d242316d1830e83788c6092183d0eb
Abdus samad <abdussamadkotha@gmail.com>
2026-07-29 17:28:14 +0500

    issue resolved

--- app/(main)/orders/page.tsx
+++ app/(main)/orders/page.tsx
@@ -1,6 +1,18 @@
 import PageHeader from "@/components/shared/PageHeader";
-import OrderWrapper from "./order-wrapper";
 import { Suspense } from "react";
+import { getOrders } from "@/lib/actions/orders/orders";
+import OrdersRealtime from "./order-client";
+
+const OrderWrapper = async () => {
+  const orders = await getOrders({});
+
+  const serializedOrders = orders.map((order) => ({
+    ...order,
+    createdAt: order.createdAt.toISOString(),
+  }));
+
+  return <OrdersRealtime initialOrders={serializedOrders} />;
+};
 
 export default async function OrdersPage() {
   return (
